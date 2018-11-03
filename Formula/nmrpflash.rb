e915e05524c341de2bf08b5b6b85e4ab1d18e567
Matt Meissner <meissnem@gmail.com>
2018-11-03 16:19:43 -0500

    Fix bad mkdir in last revision

--- Formula/nmrpflash.rb
+++ Formula/nmrpflash.rb
@@ -5,7 +5,7 @@ class Nmrpflash < Formula
   sha256 "33cdb7cae2083e7f45f709235e379f8a77fa0bcca51c0f7aafbf322a54d2a154"
 
   def install
-    mkdir(@bin)
+    mkdir_p prefix/"bin"
     system "make", "PREFIX=#{prefix}", "install"
   end
 
