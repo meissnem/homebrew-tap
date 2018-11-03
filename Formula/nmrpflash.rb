98d78d9e7ad0e4a57094246a1bcbf01b8f73ed8d
Matt Meissner <meissnem@gmail.com>
2018-11-03 16:15:08 -0500

    Add formula for nmrpflash

---
+++ Formula/nmrpflash.rb
@@ -0,0 +1,15 @@
+class Nmrpflash < Formula
+  desc "Netgear Unbrick Utility"
+  homepage "https://github.com/jclehner/nmrpflash"
+  url "https://github.com/jclehner/nmrpflash/archive/v0.9.13.tar.gz"
+  sha256 "33cdb7cae2083e7f45f709235e379f8a77fa0bcca51c0f7aafbf322a54d2a154"
+
+  def install
+    mkdir(@bin)
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
