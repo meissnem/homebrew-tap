07cfd0a263851090dd6096953341455d60f462d0
Matt Meissner <meissnem@gmail.com>
2019-10-09 16:44:01 -0500

    Ran through `brew cask style` and `brew cask audit`

--- Casks/booxter.rb
+++ Casks/booxter.rb
@@ -2,9 +2,9 @@ cask 'booxter' do
   version '2.7.19'
   sha256 '2c94f1f6b28264b3a5822d9f12ee621d5aabb1c70aaa569e3572a4890260a773'
 
-  url "http://www.deepprose.com/download/Booxter.dmg"
+  url 'http://www.deepprose.com/download/Booxter.dmg'
   name 'Booxter'
-  homepage 'http://www.deepprose.com'
+  homepage 'http://www.deepprose.com/'
 
   app 'Booxter.app'
 end
--- Casks/twitterrific.rb
+++ Casks/twitterrific.rb
@@ -1,9 +1,9 @@
 cask 'twitterrific' do
-  version "5.4.3"
+  version '5.4.3+134'
   sha256 'ff292fadea9fb820bcb2c6117b098dbb1513a23ac884a9231f4d2f89ea6a5a8c'
 
-  # https://downloads.iconfactory.com/phoenix was verified as official when first introduced to the cask
-  url "https://downloads.iconfactory.com/phoenix/Twitterrific-5.4.3+134.zip"
+  # downloads.iconfactory.com/phoenix/Twitterrific- was verified as official when first introduced to the cask
+  url "https://downloads.iconfactory.com/phoenix/Twitterrific-#{version}.zip"
   appcast 'https://iconfactory.com/appcasts/Phoenix/appcast.xml'
   name 'Twitterrific'
   homepage 'http://twitterrific.com/mac'
