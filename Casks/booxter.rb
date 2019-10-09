69e2528f0bf302e0745631fbb58b839bf1bff074
Matt Meissner <meissnem@gmail.com>
2019-10-09 16:46:50 -0500

    Get version-specific dmg

--- Casks/booxter.rb
+++ Casks/booxter.rb
@@ -2,7 +2,7 @@ cask 'booxter' do
   version '2.7.19'
   sha256 '2c94f1f6b28264b3a5822d9f12ee621d5aabb1c70aaa569e3572a4890260a773'
 
-  url 'http://www.deepprose.com/download/Booxter.dmg'
+  url "http://www.deepprose.com/download/Booxter_#{version}.dmg"
   name 'Booxter'
   homepage 'http://www.deepprose.com/'
 
