d57e54a9ccfada531c31acdcc985261cf423b34b
Matt Meissner <meissnem@gmail.com>
2019-10-24 10:56:12 -0500

    Add Optimus Player cask

---
+++ Casks/optimus-player.rb
@@ -0,0 +1,10 @@
+cask 'optimus-player' do
+  version '1.1'
+  sha256 '0484e7b3bfccf438328679cadde606763c491fc12627fa2d86e6b392f493fa32'
+
+  url "https://download.optimusplayer.com/Optimus%20Player%20#{version}.dmg"
+  name 'Optimus Player'
+  homepage 'https://www.optimusplayer.com'
+
+  app 'Optimus Player.app'
+end
