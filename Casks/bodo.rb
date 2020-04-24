9762f324fdbf655564ceae6ced902e38045dffb1
Matt Meissner <meissnem@gmail.com>
2020-04-24 02:21:38 -0500

    update version of Bodo

--- Casks/bodo.rb
+++ Casks/bodo.rb
@@ -1,6 +1,6 @@
 cask 'bodo' do
-  version '0.3.12'
-  sha256 '970148547ecd53f0d646d100109ec36c25f961de0ef0fe20fceda07decc92817'
+  version '0.5.2'
+  sha256 '561e13d57caa8aff60fbd020607e22908ba08594135f7501655a55d74f06eaea'
 
   url "https://download.getbodo.com/Bodo_#{version}.dmg"
   appcast 'https://download.getbodo.com/appcast.xml'
