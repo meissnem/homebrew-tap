750fb5b5c5ddbd90050cb1e7690ea5043f8996cd
Matt Meissner <meissnem@gmail.com>
2020-04-24 01:33:26 -0500

    add font-sf-mono-nerd-font

---
+++ Casks/font-sf-mono-nerd-font.rb
@@ -0,0 +1,21 @@
+cask 'font-sf-mono-nerd-font' do
+  version '15.0d5e1'
+  sha256 '4396b843b62a55adc2f4821d83b079008e365a6fb8c9b707fc8fbe54635c9ec0'
+
+  url "file:///Users/matt/Library/Mobile%20Documents/com~apple~CloudDocs/Library/Fonts/SFMono%20Nerd%20Font.zip"
+  name 'SFMono Nerd Font'
+  homepage 'https://developer.apple.com/fonts/'
+
+  font 'SFMono Nerd Font/SFMono Bold Italic Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Bold Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Heavy Italic Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Heavy Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Light Italic Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Light Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Medium Italic Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Medium Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Regular Italic Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Regular Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Semibold Italic Nerd Font Complete.otf'
+  font 'SFMono Nerd Font/SFMono Semibold Nerd Font Complete.otf'
+end
