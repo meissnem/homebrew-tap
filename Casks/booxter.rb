54f21388cfa54088df33f1502f1f6aeb02dc2fe4
Matt Meissner <meissnem@gmail.com>
2020-07-01 14:40:43 -0500

    update Booxter

--- Casks/booxter.rb
+++ Casks/booxter.rb
@@ -1,6 +1,6 @@
 cask 'booxter' do
-  version '2.7.19'
-  sha256 '2c94f1f6b28264b3a5822d9f12ee621d5aabb1c70aaa569e3572a4890260a773'
+  version '2.8.2'
+  sha256 'e54e5197ea92d0e7027d6002b755de35c7510076e74ab880ace16a5549e277d2'
 
   url "http://www.deepprose.com/download/Booxter_#{version}.dmg"
   name 'Booxter'
