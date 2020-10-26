44e8436d332c0c7ea088cb0f1bf6e2830f0638c1
Matt Meissner <meissnem@gmail.com>
2020-10-26 15:36:08 -0500

    add checksums

--- Casks/font-new-york.rb
+++ Casks/font-new-york.rb
@@ -1,6 +1,6 @@
 cask 'font-new-york' do
   version '16.0d2e2'
-  sha256 :no_check
+  sha256 '6cf7c864a9e4c556f52cadaff03133d81530895833dee03ddc20889c481eeb0f'
 
   url 'https://devimages-cdn.apple.com/design/resources/download/NY-Font.dmg'
   name 'New York'
--- Casks/font-sf-mono.rb
+++ Casks/font-sf-mono.rb
@@ -1,6 +1,6 @@
 cask 'font-sf-mono' do
   version '16.0d2e1'
-  sha256 :no_check
+  sha256  'd91ed5d03a249b515dd1ba9aba7127dcd85bd1c7d2a3a7687531524b6d9a6a6d'
 
   url 'https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg'
   name 'SF Mono'
