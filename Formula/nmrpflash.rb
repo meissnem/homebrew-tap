bf4d7fcf1b7e1c03eff819b101e8527e076e387c
Matt Meissner <meissnem@gmail.com>
2020-11-28 03:29:23 -0600

    add bottle

--- Formula/nmrpflash.rb
+++ Formula/nmrpflash.rb
@@ -4,6 +4,12 @@ class Nmrpflash < Formula
   url "https://github.com/jclehner/nmrpflash/archive/v0.9.13.tar.gz"
   sha256 "33cdb7cae2083e7f45f709235e379f8a77fa0bcca51c0f7aafbf322a54d2a154"
 
+  bottle do
+    root_url "https://github.com/meissnem/homebrew-meissnem/releases/download/nmrpflash-0.9.13"
+    cellar :any_skip_relocation
+    sha256 "123d158f3d96d2ebf2cc9fd043ebe04384eff4acbbc9592c5a5929079e486097" => :big_sur
+  end
+
   def install
     mkdir_p prefix/"bin"
     system "make", "PREFIX=#{prefix}", "install"
