22056002bb7d63a3b9e7f34a2675607382b28f1d
Matt Meissner <meissnem@gmail.com>
2020-12-10 11:14:25 -0600

    Update Mimestream to 0.8.0

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -1,9 +1,9 @@
 cask "mimestream" do
-  version "0.7.7"
-  sha256 "ba5785ab84f885ac7e0edd16f04b25e32d3faf1fdbdc1dd1987141a3beb29a09"
+  version "0.8.0"
+  sha256 "0e9022e5cad472160ab8d1da31bafa08162f37aef611ac2a954813203cbc6a44"
 
-  # storage.googleapis.com was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg"
+  url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg",
+      verified: "storage.googleapis.com/mimestream-releases"
   appcast "https://mimestream.com/appcast.xml"
   name "Mimestream"
   desc "Native email client for Gmail"
