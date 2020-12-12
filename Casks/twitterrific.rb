c471e59ebbdcd22ff8df2ff5fb79e8e2239e7c0d
Matt Meissner <meissnem@gmail.com>
2020-12-12 15:41:37 -0600

    Cleanup and audit fixes

--- Casks/twitterrific.rb
+++ Casks/twitterrific.rb
@@ -2,8 +2,8 @@ cask "twitterrific" do
   version "5.4.5+151"
   sha256 "ca03e8b989a16cfb88cb86c776c42a7c88110ba1c9db5adf3b55fe15b85e3e57"
 
-  # downloads.iconfactory.com/phoenix/Twitterrific- was verified as official when first introduced to the cask
-  url "https://downloads.iconfactory.com/phoenix/Twitterrific-#{version}.zip"
+  url "https://downloads.iconfactory.com/phoenix/Twitterrific-#{version}.zip",
+    verified: "downloads.iconfactory.com/phoenix"
   appcast "https://iconfactory.com/appcasts/Phoenix/appcast.xml"
   name "Twitterrific"
   desc "Twitter client"
