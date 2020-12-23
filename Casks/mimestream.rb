860f91ba3e957f77d11a721c17e7bab9b96ea5c2
Matt Meissner <meissnem@gmail.com>
2020-12-23 13:40:20 -0600

    version bump for Mimestream

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -1,6 +1,6 @@
 cask "mimestream" do
-  version "0.8.2"
-  sha256 "3da2a7f671645a2201476678180f4ed7558b008ede7351b3fb67eda55df0d4e7"
+  version "0.8.3"
+  sha256 "631b3d808a6540e3244eb8da28ad795ba638d7c86fc728c000fa78d61c04f5a2"
 
   url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg",
       verified: "storage.googleapis.com/mimestream-releases"
