39f732a6c0fe1058359ef304d455b90da1c91942
Matt Meissner <meissnem@gmail.com>
2020-12-30 11:58:05 -0600

    update mimestream to 0.8.4

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -1,6 +1,6 @@
 cask "mimestream" do
-  version "0.8.3"
-  sha256 "631b3d808a6540e3244eb8da28ad795ba638d7c86fc728c000fa78d61c04f5a2"
+  version "0.8.4"
+  sha256 "6c05505f1ef6b72a1d79bd600f5a574b9b915f20672a21b7cb3bad963c2e5b4f"
 
   url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg",
       verified: "storage.googleapis.com/mimestream-releases"
