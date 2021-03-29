fee3f1f99c497d9fb4133fa2ac19c8add4af0047
Matt Meissner <meissnem@gmail.com>
2021-03-28 19:18:46 -0500

    update NetNewsWire 6.0.1b1

--- Casks/netnewswire-beta.rb
+++ Casks/netnewswire-beta.rb
@@ -1,8 +1,8 @@
-cask "netnewswire-at-6" do
-  version "6.0b4,6024"
-  sha256 "d796d90e87df8388495a39455dd7ff4a5f3ac81599455586b0677b88beb729cf"
+cask "netnewswire-beta" do
+  version "6.0.1b1,6028"
+  sha256 "e73ba43fd587e14de32b055787100c8e9948da29e5088d28f7779b4be845e58f"
 
-  url "https://github.com/Ranchero-Software/NetNewsWire/releases/download/mac-#{version.before_comma.split("b").first}beta#{version.before_comma.split("b").last}/NetNewsWire#{version.before_comma}.zip",
+  url "https://github.com/Ranchero-Software/NetNewsWire/releases/download/mac-#{version.before_comma}/NetNewsWire#{version.before_comma}.zip",
       verified: "https://github.com/Ranchero-Software/NetNewsWire/"
   name "NetNewsWire"
   desc "Free and open-source RSS reader"
