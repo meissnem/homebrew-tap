2746b8a465bc49562397f9052ac9293f4bbebe9c
Matt Meissner <meissnem@gmail.com>
2021-05-25 00:46:04 -0500

    Bump Mimestream to 0.20.3

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -1,6 +1,6 @@
 cask "mimestream" do
-  version "0.14.4,135"
-  sha256 "bd73fe6225d52bee59bfb50936c59432f2e1ace80e4860bb9059a84556742218"
+  version "0.20.3,148"
+  sha256 "f89d299c2144a51a216dd4069ac2260d9c540ed848c779c931691b9b72bfc112"
 
   url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version.before_comma}.dmg",
       verified: "storage.googleapis.com/mimestream-releases"
