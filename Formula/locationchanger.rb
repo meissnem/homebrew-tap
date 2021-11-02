2e816a097b7a3419372e4209bf14bac8c1d12e8a
Matt Meissner <meissnem@gmail.com>
2021-11-02 19:09:32 +0000

    locationchanger: add 0.1-1 bottle.

--- Formula/locationchanger.rb
+++ Formula/locationchanger.rb
@@ -5,6 +5,12 @@ class Locationchanger < Formula
   version "0.1-1"
   sha256 "1107e5b701ff2f76e482787f6e7e714db2aad0328b3902bb84d8c61d94bb26e6"
 
+  bottle do
+    root_url "https://github.com/meissnem/homebrew-tap/releases/download/locationchanger-0.1-1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina: "5d0ce60cd11035a117482dbcbb6f39433498f6507ea91656bef9b8dc5afb24bc"
+  end
+
   depends_on :macos
 
   def install
