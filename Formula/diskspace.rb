5be5cab1f8eadcf1cec4d7c2995187d3ee0caea8
Matt Meissner <meissnem@gmail.com>
2022-01-24 10:49:06 -0600

    add `diskspace` formula

---
+++ Formula/diskspace.rb
@@ -0,0 +1,18 @@
+class Diskspace < Formula
+  desc "macOS command-line tool to return the available disk space on APFS volumes"
+  homepage "https://github.com/scriptingosx/diskspace"
+  url "https://github.com/scriptingosx/diskspace/releases/download/v1/diskspace-1.pkg"
+  sha256 "918e9796e3390b2644efd4deb36d4095599be5372fddc8536f91bfeba01f66ee"
+  license "Apache-2.0"
+
+  depends_on :macos
+
+  def install
+    system "pkgutil", "--expand-full", "diskspace-1.pkg", "tmp"
+    bin.install "tmp/Payload/usr/local/bin/diskspace"
+  end
+
+  test do
+    system "false"
+  end
+end
