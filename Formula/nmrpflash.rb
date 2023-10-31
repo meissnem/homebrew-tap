4838eb963c0e1f019a92e17bd0488dbb8cc2847a
Matt Meissner <meissnem@gmail.com>
2023-10-30 19:36:26 -0500

    nmrpflash@0.9.22

--- Formula/nmrpflash.rb
+++ Formula/nmrpflash.rb
@@ -1,15 +1,8 @@
 class Nmrpflash < Formula
   desc "Netgear Unbrick Utility"
   homepage "https://github.com/jclehner/nmrpflash"
-  url "https://github.com/jclehner/nmrpflash/archive/v0.9.16.tar.gz"
-  sha256 "ccb5974a9574f0ce361f8d2d68f743957a44c9e27dc9490589e8f91c9f8f6bb6"
-
-  bottle do
-    root_url "https://github.com/meissnem/homebrew-tap/releases/download/nmrpflash-0.9.16"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "58008d54e490f2a91e5459590d2690baa432f88d0e021fb31410fe1210926b50"
-    sha256 cellar: :any_skip_relocation, catalina:       "c07a37fadecd4b0890b98cfb9cf02b068745d24bde4c03a9ebd0f24ad741036f"
-  end
+  url "https://github.com/jclehner/nmrpflash/archive/refs/tags/v0.9.22.tar.gz"
+  sha256 "cef3b54c798a4a049a2d9b959e1d6a0ac2f4f31b802d6be4f79351b9a96c3f39"
 
   depends_on :macos
 
