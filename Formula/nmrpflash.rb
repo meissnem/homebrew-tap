8645c8c4b526294f07d97423e78a113ec1232715
Matt Meissner <meissnem@gmail.com>
2023-10-31 00:49:39 +0000

    nmrpflash: add 0.9.22 bottle.

--- Formula/nmrpflash.rb
+++ Formula/nmrpflash.rb
@@ -4,6 +4,11 @@ class Nmrpflash < Formula
   url "https://github.com/jclehner/nmrpflash/archive/refs/tags/v0.9.22.tar.gz"
   sha256 "cef3b54c798a4a049a2d9b959e1d6a0ac2f4f31b802d6be4f79351b9a96c3f39"
 
+  bottle do
+    root_url "https://github.com/meissnem/homebrew-tap/releases/download/nmrpflash-0.9.22"
+    sha256 cellar: :any_skip_relocation, ventura: "3d9b163816b7160ad2480c4c38deb787762c3d06c2294b55c58a9d69982a49b2"
+  end
+
   depends_on :macos
 
   def install
